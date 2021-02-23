d37d69d12e55c50d3f3d28784ee8d895f505d608
Austin Garrod <austin@austingarrod.ca>
2021-02-23 17:46:08 -0500

    documented app.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+// Import libraries and components
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -5,15 +6,24 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// Import screens
 import TableScreen from './src/screens/table';
 import DetailsScreen from './src/screens/details';
 
+// Import typescript values
 import { ScreenStackParams } from './src/typescript/screenparams';
 import { Screens } from './src/typescript/screens';
 
+// Create a navigation stack
 const Stack = createStackNavigator<ScreenStackParams>();
 
+/**
+ * Main app componenet to hold screens for navigation
+ */
 class App extends Component {
+  /**
+   * Render method to return TSX
+   */
   render(){
     return (
       <PaperProvider>
@@ -39,11 +49,5 @@ class App extends Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  main: {
-    marginTop: 30
-  }
-});
 
 export default App;
