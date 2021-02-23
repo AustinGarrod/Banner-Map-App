36499efa0278404fc1a79ea498679c58157af89c
Austin Garrod <austin@austingarrod.ca>
2021-02-23 17:53:57 -0500

    Documemented table.tsx

--- src/screens/table.tsx
+++ src/screens/table.tsx
@@ -1,20 +1,41 @@
+// Import libraries and components
 import React, { Component } from 'react';
 import { ScrollView, TouchableOpacity } from 'react-native';
 import { DataTable } from 'react-native-paper';
 import { StackNavigationProp } from '@react-navigation/stack';
 
+// Import custom component
 import TableRow from '../components/tableRow';
 
+//  Import banner data from local file
 import bannerData from '../data/banners.json';
+
+// Import typescript values
 import { ScreenStackParams } from '../typescript/screenparams';
 import { Screens } from '../typescript/screens';
 
-
+// Define props for TableScreen componenet
 type Props = {
   navigation: StackNavigationProp<ScreenStackParams, Screens.TableScreen>
 }
 
+/**
+ * TableScreen componenet to display table of all veterans
+ */
 class TableScreen extends Component<Props> {
+  /**
+   * Constructor for componenent
+   * @param props Props passed to component
+   */
+  constructor(props: Props){
+    super(props);
+
+    // TODO: Create banner filter
+  }
+
+  /**
+   * Render method to return TSX
+   */
   render(){
     return (
       <ScrollView>
@@ -25,7 +46,7 @@ class TableScreen extends Component<Props> {
             <DataTable.Title>Branch</DataTable.Title>
           </DataTable.Header>
 
-          {
+          { // Loop through rows of veterans, creating new row for each
             bannerData.slice(0, 20).map((banner, index) => {
               return <TableRow key={index} navigation={this.props.navigation} banner={banner} />;
             })
@@ -37,4 +58,5 @@ class TableScreen extends Component<Props> {
   }
 }
 
+// Export TableScreen componenet
 export default TableScreen;
