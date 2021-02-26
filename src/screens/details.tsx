184427417298b3796ba7eb816c7a7106d987238d
Austin Garrod <austin@austingarrod.ca>
2021-02-26 17:01:13 -0500

    Implemented map on details page

--- src/screens/details.tsx
+++ src/screens/details.tsx
@@ -1,11 +1,13 @@
 // Import libraries and components
 import React, { Component } from 'react';
-import { ScrollView, View, StyleSheet, Text } from 'react-native';
+import { ScrollView, View, StyleSheet, Text, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import { Card } from 'react-native-paper'
 import Banner from '../typescript/interfaces/banner';
 
 // Import componenets
 import DetailsRow from '../components/detailsRow'
+import Map from '../components/map';
 
 // Define props for DetailsScreen componenet
 type Props = {
@@ -14,10 +16,18 @@ type Props = {
   } };
 }
 
+type Region = {
+  latitude: Number,
+  longitude: Number,
+  latitudeDelta: Number,
+  longitudeDelta: Number
+}
+
 /**
  * Details screen componenet to display individual veteran details
  */
 class DetailsScreen extends Component<Props> {
+
   /**
    * Render method to return TSX
    */
@@ -26,6 +36,18 @@ class DetailsScreen extends Component<Props> {
 
     return (
       <ScrollView>
+
+        <Map 
+          region={{
+            latitude: details.lat,
+            longitude: details.long,
+            longitudeDelta: 0.01,
+            latitudeDelta: 0.01
+          }}
+
+          markers={[{latitude: details.lat, longitude: details.long}]}
+         />
+
         <Card>
           <Card.Content>
             <DetailsRow title="Name" value={`${details.firstName} ${details.lastName}`} />
@@ -38,6 +60,19 @@ class DetailsScreen extends Component<Props> {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height / 2,
+  },
+});
 
 // Export the details screen componenet
 export default DetailsScreen;
